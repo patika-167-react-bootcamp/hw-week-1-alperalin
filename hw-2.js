5a4bce66ccc53d52797519b149bc3044f97405ba
alperalin <alperalin@gmail.com>
2022-02-08 15:42:37 +0300

    - HW-2 icin type kontrolu eklendi.

--- hw-2.js
+++ hw-2.js
@@ -1,11 +1,17 @@
 Array.prototype.includesCi = function (sItem, index = 0) {
-	// Orjinal Array'in ogeleri, Array'i bozmamak icin
+	// Orjinal Array'in String ogeleri, Array'i bozmamak icin
 	// lowercase edilip gecici bir array'a aktariliyor
-	const tempArr = this.map((item) => item.toLowerCase());
+	const tempArr = this.map((item) =>
+		typeof item === 'string' ? item.toLowerCase() : item
+	);
+
+	// Array'de aranan degere type kontrolu yapiliyor.
+	// Eger String ise lowercase ediliyor.
+	sItem = typeof sItem === 'string' ? sItem.toLowerCase() : sItem;
 
 	// includes methodu ile gecici array icerisinde
 	// verilen deger araniyor
-	return tempArr.includes(sItem.toLowerCase(), index);
+	return tempArr.includes(sItem, index);
 };
 
 // ORNEK 1
@@ -17,6 +23,10 @@ const arr2 = ['MehMet', 'aHmet', 'Şess', 'c', 'd'];
 console.log('ORNEK 2', arr2.includesCi('b'));
 
 // ORNEK 3
+const arr3 = ['MehMet', 'aHmet', 'Şess', 'c', 5];
+console.log('ORNEK 3', arr3.includesCi('5'));
+
+// ORNEK 4
 const access = [
 	'admin',
 	'moderator',
@@ -25,4 +35,4 @@ const access = [
 	'companyUser',
 	'plainUser',
 ];
-console.log('ORNEK 3', access.includesCi('sUpeRaDmIN'));
+console.log('ORNEK 4', access.includesCi('sUpeRaDmIN'));
