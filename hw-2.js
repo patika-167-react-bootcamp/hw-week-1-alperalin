efbcd16540a6c13718900365b6ecf0b01044904e
alperalin <alperalin@gmail.com>
2022-02-08 15:56:36 +0300

    - HW-2 icin turkce karakterli ornek eklendi

--- hw-2.js
+++ hw-2.js
@@ -1,6 +1,6 @@
 Array.prototype.includesCi = function (sItem, index = 0) {
-	// Orjinal Array'in String ogeleri, Array'i bozmamak icin
-	// lowercase edilip gecici bir array'a aktariliyor
+	// Orjinal Array'in ogeleri, Array'i bozmamak icin
+	// String'ler lowercase edilip gecici bir array'a aktariliyor
 	const tempArr = this.map((item) =>
 		typeof item === 'string' ? item.toLowerCase() : item
 	);
@@ -24,9 +24,13 @@ console.log('ORNEK 2', arr2.includesCi('b'));
 
 // ORNEK 3
 const arr3 = ['MehMet', 'aHmet', 'Şess', 'c', 5];
-console.log('ORNEK 3', arr3.includesCi('5'));
+console.log('ORNEK 3', arr3.includesCi(5));
 
 // ORNEK 4
+const arr4 = ['MehMet', 'aHmet', 'Çalıkuşu', 'c', 5];
+console.log('ORNEK 4', arr4.includesCi('çalıkuşu'));
+
+// ORNEK 5
 const access = [
 	'admin',
 	'moderator',
@@ -35,4 +39,4 @@ const access = [
 	'companyUser',
 	'plainUser',
 ];
-console.log('ORNEK 4', access.includesCi('sUpeRaDmIN'));
+console.log('ORNEK 5', access.includesCi('sUpeRaDmIN'));
